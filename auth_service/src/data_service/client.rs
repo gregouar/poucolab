3beddefa89a21f79d5e78173433b0a309713ef74
Grégoire Naisse <gregouar08@gmail.com>
2025-07-27 19:43:20 +0200

    minor changes

--- auth_service/src/data_service/client.rs
+++ auth_service/src/data_service/client.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use futures::TryStreamExt;
-use reqwest::header;
+use http::Method;
+use reqwest::{header, Body};
 use serde::de::DeserializeOwned;
 use shared::rest::test_api::GetTestDataResponse;
 use std::sync::Arc;
@@ -39,57 +40,71 @@ impl DataServiceClient {
         self.get_stream("/data").await
     }
 
-    // TODO: make generic over method and then create handy shortcuts for GET, POST, etc.
-
     // For typed responses (normal API calls)
     async fn get_typed<T>(&self, path: &str) -> Result<T>
     where
         T: DeserializeOwned,
     {
-        let response = self
-            .client
-            .get(format!("{}{}", self.base_url, path))
-            .send()
-            .await?;
-
-        if response.status().is_success() {
-            Ok(response.json().await?)
-        } else {
-            Err(anyhow::anyhow!("failed to fetch data from {}", path))
-        }
+        Ok(self.get(path).await?.json().await?)
     }
 
     // For streaming responses (large datasets)
     async fn get_stream(&self, path: &str) -> Result<StreamResponse> {
-        let response = self
+        let response = self.get(path).await?;
+
+        let content_type = response
+            .headers()
+            .get(header::CONTENT_TYPE)
+            .and_then(|v| v.to_str().ok())
+            .unwrap_or("application/json")
+            .to_string();
+
+        let content_length = response
+            .headers()
+            .get(header::CONTENT_LENGTH)
+            .and_then(|v| v.to_str().ok())
+            .and_then(|v| v.parse().ok());
+
+        let stream = Box::new(response.bytes_stream().map_err(anyhow::Error::from));
+
+        Ok(StreamResponse {
+            stream,
+            content_type,
+            content_length,
+        })
+    }
+
+    async fn get(&self, path: &str) -> Result<reqwest::Response> {
+        self.request(Method::GET, path, None).await
+    }
+
+    // async fn post(&self, path: &str, body: Body) -> Result<reqwest::Response> {
+    //     self.request(Method::GET, path, Some(body)).await
+    // }
+
+    async fn request(
+        &self,
+        method: Method,
+        path: &str,
+        body: Option<Body>,
+    ) -> Result<reqwest::Response> {
+        let mut request = self
             .client
-            .get(format!("{}{}", self.base_url, path))
-            .send()
-            .await?;
+            .request(method, format!("{}{}", self.base_url, path));
+
+        if let Some(body) = body {
+            request = request.body(body);
+        }
+
+        let response = request.send().await?;
 
         if response.status().is_success() {
-            let content_type = response
-                .headers()
-                .get(header::CONTENT_TYPE)
-                .and_then(|v| v.to_str().ok())
-                .unwrap_or("application/json")
-                .to_string();
-
-            let content_length = response
-                .headers()
-                .get(header::CONTENT_LENGTH)
-                .and_then(|v| v.to_str().ok())
-                .and_then(|v| v.parse().ok());
-
-            let stream = Box::new(response.bytes_stream().map_err(anyhow::Error::from));
-
-            Ok(StreamResponse {
-                stream,
-                content_type,
-                content_length,
-            })
+            Ok(response)
         } else {
-            Err(anyhow::anyhow!("failed to fetch data from {}", path))
+            Err(anyhow::anyhow!(
+                "failed to contact data service on {}",
+                path
+            ))
         }
     }
 }
